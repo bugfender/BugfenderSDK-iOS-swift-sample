c561cd114373dbd2d8e8b14d6667458a51891ae9
Aleix Ventayol <aleix@ventayol.cat>
2016-10-10 17:46:15 +0200

    Added Err and Warn functions to Swift SDK

--- BugfenderSDK-iOS-swift-sample/BugfenderSwift.swift
+++ BugfenderSDK-iOS-swift-sample/BugfenderSwift.swift
@@ -16,3 +16,21 @@ func BFLog(_ message: String, filename: String = #file, line: Int = #line, funcn
         NSLog("[\(file):\(line)] \(funcname) - %@", message)
     #endif
 }
+
+func BFLogErr(_ message: String, filename: String = #file, line: Int = #line, funcname: String = #function) {
+    let file = ("\(filename)" as NSString).lastPathComponent as String
+    
+    Bugfender.logLineNumber(line, method: funcname, file: file, level: BFLogLevel.error, tag: nil, message: message)
+    #if DEBUG
+        NSLog("[\(file):\(line)] \(funcname) - %@", message)
+    #endif
+}
+
+func BFLogWarn(_ message: String, filename: String = #file, line: Int = #line, funcname: String = #function) {
+    let file = ("\(filename)" as NSString).lastPathComponent as String
+    
+    Bugfender.logLineNumber(line, method: funcname, file: file, level: BFLogLevel.warning, tag: nil, message: message)
+    #if DEBUG
+        NSLog("[\(file):\(line)] \(funcname) - %@", message)
+    #endif
+}
